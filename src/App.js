e1d09d2def67a2caebeb8eaee0f2b218fe898127
Rafael Danon <rafaeldanon@npi4027fa.nai.local>
2021-12-13 06:56:51 +0200

    deploy test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './components/Login';
+require('dotenv').config();
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-require('dotenv').config();
 
 const Login = () => {
   const [email, setEmail] = useState('admin@admin.com');
